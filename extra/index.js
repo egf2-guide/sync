15db70ef3acce1ae04be5c43af2aa29301a756b0
Alexander Demidov <alex.s.demidov@gmail.com>
2016-12-09 15:13:35 +0700

    added post_user extra handlers

--- extra/index.js
+++ extra/index.js
@@ -2,5 +2,11 @@
 
 // add "<POST, PUT, DELETE> <object type or edge>": <handler function (event)>
 const handleRegistry = {
+    "POST user": require("./post_user").onPost,
+    "PUT user": require("./post_user").onPut,
+    "DELETE user": require("./post_user").onDelete,
+    "POST post": require("./post_user").onPost,
+    "PUT post": require("./post_user").onPut,
+    "DELETE post": require("./post_user").onDelete
 };
 module.exports = handleRegistry;
---
+++ extra/post_user.js
@@ -0,0 +1,60 @@
+"use strict";
+
+const config = require("../components").config;
+const elastic = require("../components").elasticSearch;
+
+const indexName = "post_user";
+
+function isUser(event) {
+    let obj = event.current || event.previous;
+    return obj.object_type === "user";
+}
+
+function onPost(event) {
+    return elastic.index({
+        index: indexName,
+        type: indexName,
+        id: event.object,
+        body: {
+            id: event.current.id,
+            combo: isUser(event) ?
+                `${event.current.name.given} ${event.current.name.family}` :
+                event.current.description
+        }
+    });
+}
+
+function onPut(event) {
+    let combo;
+    if (isUser(event)) {
+        if (event.current.name.given !== event.previous.name.given ||
+            event.current.name.family !== event.previous.name.family) {
+            combo = `${event.current.name.given} ${event.current.name.family}`;
+        }
+    } else if (event.current.description !== event.previous.description) {
+        combo = event.current.description;
+    }
+    if (combo) {
+        return elastic.update({
+            index: indexName,
+            type: indexName,
+            id: event.object,
+            body: {combo}
+        });
+    }
+    return Promise.resolve();
+}
+
+function onDelete(event) {
+    return elastic.delete({
+        index: indexName,
+        type: indexName,
+        id: event.object
+    });
+}
+
+module.exports = {
+    onPost,
+    onPut,
+    onDelete
+};
